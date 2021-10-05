151688185807f449abd4c0ba73d19f1f32968d4f
colin oneill <coneill65@students.mchenry.edu>
2021-10-04 22:33:54 -0500

    Initial commit

---
+++ app/src/main/java/com/example/appbartry2/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.appbartry2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // Note that the Toolbar defined in the layout has the id "my_toolbar"
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+        supportActionBar?.title = "test"
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+
+        R.id.action_favorite -> {
+            // User chose the "Favorite" action, mark the current item
+            // as a favorite...
+            true
+        }
+
+        else -> {
+            // If we got here, the user's action was not recognized.
+            // Invoke the superclass to handle it.
+            super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+}
